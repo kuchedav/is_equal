43e5480d02ee702138592f3cd371425606faf2e1
kuchedav <kuchelmeister.david@gmail.com>
2020-07-06 18:28:05 +0200

    initial commit

---
+++ is_equal.py
@@ -0,0 +1,37 @@
+def is_equal_recursive(elem1,elem2):
+    if(type(elem1) != type(elem2)):
+        return False
+    if(isinstance(elem1,list)):
+        return [is_equal_recursive(i,j) for i,j in zip(elem1,elem2)]
+    elif(isinstance(elem1,dict)):
+        elem1_in = list(elem1.values())
+        elem2_in = list(elem2.values())
+        return [is_equal_recursive(i,j) for i,j in zip(elem1_in,elem2_in)]
+    elif(isinstance(elem1,int) or isinstance(elem1,float) or isinstance(elem1,str)):
+        return elem1 == elem2
+    elif "pandas" in str(type(elem1)):
+        return elem1.equals(elem2)
+
+def list_flatten(l, a=None):
+    if a is None: a = []
+    for i in l:
+        if isinstance(i, list):
+            list_flatten(i, a)
+        else:
+            a.append(i)
+    return a
+
+def is_equal(elem1,elem2):
+    boolen_list = is_equal_recursive(elem1,elem2)
+    return all(list_flatten(boolen_list))
+
+if __name__=="__main__":
+    import pandas as pd
+    import numpy as np
+    df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),columns=['a', 'b', 'c'])
+
+    test1 = [{"a":1,"b":2}, 10, [1,2,[4,5,6]], df2]
+    test2 = [{"a":1,"b":2}, 11, [1,2,[4,5,6]], df2]
+    
+    out = is_equal(test1,test2)
+    print(out)

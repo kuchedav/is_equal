172df8b9618b8473a3b17a5c30d5eed6fe3d3768
kuchedav <kuchelmeister.david@gmail.com>
2020-07-08 14:29:47 +0200

    General overhaul over most of the functions

--- is_equal.py
+++ is_equal.py
@@ -1,16 +1,27 @@
+import pandas as pd
+import numpy as np
+
 def is_equal_recursive(elem1,elem2):
     if(type(elem1) != type(elem2)):
         return False
     if(isinstance(elem1,list)):
+        if len(elem1) != len(elem2):
+            return False
         return [is_equal_recursive(i,j) for i,j in zip(elem1,elem2)]
     elif(isinstance(elem1,dict)):
+        if len(elem1) != len(elem2):
+            return False
         elem1_in = list(elem1.values())
         elem2_in = list(elem2.values())
         return [is_equal_recursive(i,j) for i,j in zip(elem1_in,elem2_in)]
     elif(isinstance(elem1,int) or isinstance(elem1,float) or isinstance(elem1,str)):
         return elem1 == elem2
     elif "pandas" in str(type(elem1)):
+        if elem1.shape != elem2.shape:
+            return False
         return elem1.equals(elem2)
+    elif "sklearn" in str(type(elem1)):
+        return str(elem1) == str(elem2)
 
 def list_flatten(l, a=None):
     if a is None: a = []
@@ -21,9 +32,11 @@ def list_flatten(l, a=None):
             a.append(i)
     return a
 
-def is_equal(elem1,elem2):
+def is_equal(elem1,elem2,flatten = True):
     boolen_list = is_equal_recursive(elem1,elem2)
-    return all(list_flatten(boolen_list))
+    
+    if flatten: return all(list_flatten(boolen_list))
+    else: return boolen_list
 
 if __name__=="__main__":
     import pandas as pd
